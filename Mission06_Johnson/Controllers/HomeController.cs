4f86509353fe0aeb690562d26e89958dee1b5c6d
Micah <mjmikes@byu.edu>
2025-02-10 09:18:39 -0700

    Joels picture

--- Mission06_Johnson/Controllers/HomeController.cs
+++ Mission06_Johnson/Controllers/HomeController.cs
@@ -18,14 +18,15 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult GetToKnowJoel()
+    {
+        return View();
+    }
+    
+    public IActionResult Movies()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
 }
